8731274baebae400e039edf9959c1eef5e9a8851
Zehua Yu <zehua.yu@columbia.edu>
2022-09-16 03:55:34 +0800

    Update usage_example_python.py

--- usage_example_python.py
+++ usage_example_python.py
@@ -23,12 +23,15 @@ print(arr)  # same result as C++.
 np.random.seed(0)
 arr = np.random.randn(20, 240, 5000).astype('float32')  # explicitly cast to float32
 print(arr[10, :20, 42])  # check the first 20 minutes of stock no.43 on day 11
+
 # [-0.31888023 -0.19876899 -0.668215    1.2044029   1.0545355  -1.6606108
 #  -1.1592734   0.8667814   0.51651764 -0.17564432 -0.16574599  0.92819685
 #  -0.27120432 -0.6692324   2.0230536   0.17266187 -1.3617305   0.09074531
 #   0.37932783 -0.76033247]
+
 rolling_rank = rsp.RollingRank_float()
 rsp.roll_ndarray_float(arr, rolling_rank, axis=1, window=5, min_periods=3)
 print(arr[10, :20, 42])
+
 # [nan nan  0.  3.  3.  0.  1.  2.  2.  2.  2.  4.  0.  0.  4.  2.  0.  2.
 #   3.  1.]
